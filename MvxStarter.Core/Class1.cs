3ffdda8d183b52ad557eaf90524b459a3da731a3
Nestor Mejia <nestor.mejia98@hotmail.com>
2021-09-16 19:09:10 +0200

    core code

--- MvxStarter.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MvxStarter.Core
-{
-    public class Class1
-    {
-    }
-}
---
+++ MvxStarter.Core/ViewModels/GuestBookViewModel.cs
@@ -0,0 +1,41 @@
+﻿using MvvmCross.ViewModels;
+using MvxStarter.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace MvxStarter.Core.ViewModels
+{
+    public class GuestBookViewModel : MvxViewModel
+    {
+        private ObservableCollection<PersonModel> _people = new ObservableCollection<PersonModel>();
+
+        public ObservableCollection<PersonModel> People
+        {
+            get { return _people; }
+            set { SetProperty(ref _people, value); }
+        }
+
+        private string _firstName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { SetProperty(ref _firstName, value); }
+        }
+
+        private string _lastName;
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { SetProperty(ref _lastName, value); }
+        }
+
+
+
+
+
+    }
+}
